e6f90dc0b44a635f868b3fb8aa4e81e123eed1ee
yaron2 <schneider.yaron@live.com>
2017-07-08 17:30:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use(express.static(path.join(__dirname, 'site')))
+
+app.get('/', function (req, res) {
+  res.sendFile("index.html")
+})
+
+var port = process.env.PORT || 3000
+
+app.listen(port, function () {
+  console.log('Captain Rogers Is Ready on port ' + port)
+})
