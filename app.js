e65624ebdc0995a978cc17af99ea09708c25f5eb
yaron2 <schneider.yaron@live.com>
2017-07-13 13:58:58 +0300

    test2

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'site')))
 
 app.get('/', function (req, res) {
-  res.sendFile("index.html")
+  res.sendFile("index.html");
 })
 
 var port = process.env.PORT || 3000
 
 app.listen(port, function () {
-  console.log("Hi Spiderfrog");
+  console.log("Hi Spiderfrog!");
   console.log('Captain Rogers Is Ready on port ' + port)
 })
