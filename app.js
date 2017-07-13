7958b5457eadff2cba5d9e0291fc460462ec82e4
yaron2 <schneider.yaron@live.com>
2017-07-13 15:21:40 +0300

    hi

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || 3000
 
 app.listen(port, function () {
-  console.log("Hi people");
+  console.log("Hi people!");
   console.log('Captain Rogers Is Ready on port ' + port)
 })
